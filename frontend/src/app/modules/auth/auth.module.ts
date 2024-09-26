6d956b5ad26b26cd99158caf0e807e4bf32c77bd
jorio-calhengue <jorio.calhengue@pedromag.com>
2024-09-26 10:20:39 +0100

    estrutura do template com modelo de pastas

--- frontend/src/app/modules/auth/auth.module.ts
+++ frontend/src/app/modules/auth/auth.module.ts
@@ -4,6 +4,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './components/login/login.component';
+import {FormsModule} from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common'; 
+
+
 
 
 @NgModule({
@@ -14,7 +21,12 @@ import { LoginComponent } from './components/login/login.component';
     AuthRoutingModule,
     MatInputModule,
     MatButtonModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    FormsModule,
+    RouterModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    CommonModule
   ]
 })
 export class AuthModule { }
--- frontend/src/app/modules/auth/components/login/login.component.ts
+++ frontend/src/app/modules/auth/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { User } from '../../model/login';
 
 @Component({
   selector: 'app-login',
@@ -6,11 +9,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  
+  formularioLogin!: FormGroup;
+  error!: string;
+  loginError: string = '';
+  submitted = false; 
+
+  constructor(
+    private fb: FormBuilder, 
+    private router: Router // Serviço de roteamento injetado
+  ) { }
 
   ngOnInit(){
-
+    this.formularioLogin = this.fb.group({
+      femail: ['',[Validators.required, Validators.email],], // Validators.email para validar formato de email
+      fsenha: ['', [Validators.required]]
+    }); 
   }
 
+  onSubmit(): void {
+    this.submitted = true;
+    
+    // Se o formulário for inválido, sai da função
+    if (this.formularioLogin.invalid) {
+      return;
+    }
+    
+    const logar: User = {
+      username: this.formularioLogin.get('femail')?.value,
+      password: this.formularioLogin.get('fsenha')?.value,
+    };
   
-
+    // Verifica se o e-mail e a senha são corretos
+    if (logar.username === 'administrator@pedromag' && logar.password === 'temp.12345') {
+      // Redireciona para a rota desejada
+      this.router.navigate(['/sysconp']);
+    } else {
+      // Se o e-mail ou senha estiverem incorretos, exibe mensagem de erro
+      this.loginError = 'Email ou senha incorretos. Tente novamente.';
+    }
+  }
+  
 }
