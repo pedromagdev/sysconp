aa1b849dd15984aa56503853dd0c573d72821809
Borgitoh <kevenconde1998@gmail.com>
2024-09-26 09:31:11 +0100

    Refatori project diretorio

---
+++ frontend/src/app/modules/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { AuthRoutingModule } from './auth-routing.module';
+import { LoginComponent } from './components/login/login.component';
+
+
+@NgModule({
+  declarations: [
+    LoginComponent
+  ],
+  imports: [
+    AuthRoutingModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule
+  ]
+})
+export class AuthModule { }
