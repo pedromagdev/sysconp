9f10b51bc86132cd1a4452bbc3871495fe7dbac0
Borgitoh <kevenconde1998@gmail.com>
2024-09-26 10:37:20 +0100

    Defenir cor no projeto

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo:'login', pathMatch:'full'},
-  { path: '**', redirectTo:'login'},
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule) },
   { path: 'sysconp', loadChildren: () => import('./modules/sysconp/sysconp.module').then(m => m.SysconpModule) },
-  { path: 'login', loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule) }
+  { path: '**', redirectTo: 'login' }
 ];
 
 @NgModule({
